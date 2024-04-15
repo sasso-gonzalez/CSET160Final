cd74f2acac2dfa8eb2b7154ed5f1d96c006f2ea4
sasso-gonzalez <assogonzalezs@gmail.com>
2024-04-14 23:16:00 -0400

    finished misc couldnt finish edit but started. Helped with delete and create.

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request, redirect  # imported flask
 from sqlalchemy import create_engine, text
 
-
-c_str = "mysql://root:MySQL8090V11@localhost/canvas2_0"
+c_str = "mysql://root:MySQL@localhost/canvas2_0"
 engine = create_engine(c_str, echo=True)
 connection = engine.connect()
 
 app = Flask(__name__)
-
 
 
 @app.route('/')
@@ -18,7 +16,7 @@ def home():
 @app.route('/acc_display', methods=['GET'])
 def get_users():
     user = connection.execute(text("select * from user")).all()  # .all() is just for retrieving/getting all the data
-    print(user)
+    # print(user)
     return render_template('acc_display.html', acc_display=user)
 
 
@@ -41,9 +39,9 @@ def regisForm():
 
 @app.route('/register', methods=['POST'])
 def registered():
-    connection.execute(text('INSERT INTO USER VALUES (:id, :name, :type, :username, :password)'), request.form)
+    connection.execute(text(f'INSERT INTO USER VALUES (:id, :name, :type, :username, :password)'), request.form)
     connection.commit()  # gives a layer of protection when someone submits a form or info to the db
-    return render_template('register.html')
+    return render_template('login.html')
 
 
 @app.route('/login', methods=['GET'])
@@ -53,8 +51,9 @@ def loginForm():
 
 @app.route('/management', methods=['GET'])
 def get_tests():
-    all_tests = connection.execute(text("select * from TESTS")).all()  # .all() is just for retrieving/getting all the data
-    print(all_tests)
+    all_tests = connection.execute(
+        text("SELECT * FROM TESTS")).all()  # .all() is just for retrieving/getting all the data
+    # print(all_tests)
     return render_template('managetests.html', management=all_tests)
 
 
@@ -71,91 +70,97 @@ def deleteForm():
 # --------------------------- START OF CREATE.HTML ---------------------------
 
 
-
 @app.route('/create', methods=['GET'])
 def createForm():
-
-
     return render_template('create.html')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # --------------------------- END OF CREATE.HTML ---------------------------
+# --------------------------- START OF EDIT.HTML ---------------------------
+
+@app.route('/edit/<TEST_ID>', methods=['GET'])
+def get_test_edit(TEST_ID):
+    test = connection.execute(text(f"SELECT TEST_ID, TEST_NAME, Q_ID, QUEST_NUM, QUESTION, ACT_ANSWER, TD_ID FROM TESTS NATURAL JOIN TEST_FORM WHERE TEST_ID = {TEST_ID};")).all()
+    print(test)
+    return render_template('edit.html', edit=test)
 
 
+@app.route('/edit/<TEST_ID>', methods=['POST'])
+def editForm(TEST_ID):
+    connection.execute(text(f'UPDATE TESTS SET TEST_NAME = (:TEST_NAME) WHERE Q_ID = (:Q_ID)'), request.form)
+    connection.execute(text(f'UPDATE TEST_FORM SET QUESTION = (:QUESTION), ACT_ANSWER = (:ACT_ANSWER) WHERE Q_ID = (:Q_ID)'),request.form)
+    connection.commit()  # gives a layer of protection when someone submits a form or info to the db
+    return render_template('/edit.html')
 
 
+# --------------------------- END OF EDIT.HTML ---------------------------
+
+# --------------------------- START OF MISCELLANEOUS ---------------------------
+@app.route('/tests_taken', methods=['GET'])
+def get_takentests():
+    usertests = connection.execute(text("SELECT * FROM TESTS")).all()
+    print(usertests)
+    return render_template('tests_taken.html', tests_taken=usertests)
 
 
+@app.route('/tests_taken', methods=['GET'])
+def displaytakentests():
+    return render_template('tests_taken.html')
 
 
+@app.route('/tests_taken', methods=['POST'])
+def searchtakenTests():  # table
+    searchtests = connection.execute(text(f'SELECT * FROM TESTS WHERE TEACH_ID = (:TEACH_ID)'), request.form)
+    print(searchtests)
+    connection.commit()
+    return render_template('tests_taken.html', tests_taken=searchtests)
 
 
+@app.route('/STU_tests/<TEST_ID>', methods=['GET'])
+def getting_taken_tests(TEST_ID):
+    gettaken = connection.execute(text(f"SELECT WHO_TESTED, SET_MARKS, TEACH_ID, NAME FROM TEST_DETAILS NATURAL JOIN TEACHER NATURAL JOIN USER WHERE TEST_ID = {TEST_ID};")).all()
+    print(gettaken)
+    return render_template('STU_tests.html', STU_tests=gettaken)
+
+@app.route('/part_student', methods=['GET'])
+def get_student():
+    PARSTU = connection.execute(text('SELECT NAME, STU_ID FROM STUDENT NATURAL JOIN USER;')).all()
+    print(PARSTU)
+    return render_template('part_student.html', part_student=PARSTU)
 
 
+@app.route('/part_student', methods=['GET'])
+def displaypartstudent():
+    return render_template('part_student.html')
 
 
+@app.route('/part_student', methods=['POST'])
+def searchingstudent():  # table
+    searchstu = connection.execute(text(f'SELECT NAME, STU_ID FROM STUDENT NATURAL JOIN USER WHERE STU_ID = (:STU_ID);'), request.form)
+    print(searchstu)
+    return render_template('part_student.html', part_student=searchstu)
 
 
+@app.route('/STU_info/<STU_ID>', methods=['GET'])
+def get_part_student(STU_ID):
+    get_par_student = connection.execute(text(f"SELECT WHO_TESTED, STU_ID, TEST_ID, TEST_NAME, SET_MARKS, TD_ID FROM STU_DETAILS NATURAL JOIN TESTS NATURAL JOIN TEST_DETAILS WHERE STU_ID = {STU_ID};")).all()
+    print(get_par_student)
+    return render_template('STU_info.html', STU_info=get_par_student)
 
+@app.route('/STU_info/<STU_ID>', methods=['POST', 'GET'])
+def update_marks(STU_ID):
+    update = connection.execute(text(f"UPDATE STU_DETAILS NATURAL JOIN TESTS NATURAL JOIN TEST_DETAILS SET SET_MARKS = (:SET_MARKS) WHERE STU_ID = {STU_ID} AND TEST_ID = (:TEST_ID)"), request.form)
+    print(update)
+    connection.commit()
+    return redirect(f'/STU_info/{STU_ID}')
 
+@app.route('/STU_2ND/<TD_ID>', methods=['GET'])
+def student_response(TD_ID):
+    student_quest = connection.execute(text(f"SELECT TD_ID, WHO_TESTED, TEST_ID, Q_ID, QUEST_NUM, QUESTION, RESPONSE FROM TEST_DETAILS NATURAL JOIN TEST_FORM WHERE TD_ID = {TD_ID};")).all()
+    print(student_quest)
+    return render_template('STU_2ND.html', STU_2ND=student_quest)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# --------------------------- END OF MISCELLANEOUS ---------------------------
 
 
 if __name__ == '__main__':
